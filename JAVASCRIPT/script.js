3120b41127780da7e4b7d0e95e0d25005b6b5a4a
ivakuz29 <ivakuz29@gmail.com>
2025-05-12 11:01:26 +0200

    Update

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,5 @@
+let backgroundButton = document.getElementById("backgroundBtn");
+
+backgroundButton.addEventListener("click", function() {
+    document.body.style.backgroundColor = "#282C50";
+});
