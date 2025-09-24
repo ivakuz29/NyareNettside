372673292b55ff84f76b357054e00e77817992fe
ivakuz29 <ivakuz29@gmail.com>
2025-09-24 10:12:51 +0200

    Commit

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,5 +1,16 @@
-let backgroundButton = document.getElementById("backgroundBtn");
-
-backgroundButton.addEventListener("click", function() {
+  let backgroundButton = document.getElementById("backgroundBtn");
+  backgroundButton.addEventListener("click", function () {
     document.body.style.backgroundColor = "#282C50";
-});
+  });
+
+  // 2) Endre overskriftstekst
+  let textButton = document.getElementById("textBtn");
+  textButton.addEventListener("click", function () {
+    document.querySelector(".javascriptfun h3").textContent = "Teksten er endret!";
+  });
+
+  // 3) Toggle dark mode
+  let toggleButton = document.getElementById("toggleBtn");
+  toggleButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+  });
